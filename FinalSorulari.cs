9de3b4cdd73ecfb792b1eeb7baf86a7f65c98438
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2023-01-29 16:12:07 +0300

    Update FinalSorulari.cs

--- FinalSorulari.cs
+++ FinalSorulari.cs
@@ -1,11 +1,13 @@
 using System;
-using System.Collections;
 using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+using System.Collections;
 using System.Windows.Forms;
 using Microsoft.Win32;
-using Microsoft.VisualBasic;
 using Hesaplama.dll;
 
+namespace WindowsFormsApp73
+{
 /*
 Formun loadında textBoxların visiblesini false yap. Registry oluştur dosya içinde dosya şeklinde ve bi şifresi olsun.
 button tıklaması ile şifre istiyor. şifre ynalışsa registry dosyalarını sil.
@@ -15,20 +17,7 @@ dll ile ortalama hesapla ve textBoxa yaz.
 button tıklaması ile veritabanına ekle.
 Button tıklaması ile rastgele bir kişinin bilgileirni textBoxlara yazdır.
 */
-
-namespace WindowsFormsApp72
-{
-    public partial class Form1 : Form
-    {
-        Formun loadında textBoxların visiblesini false yap. Registry oluştur dosya içinde dosya şeklinde ve bi şifresi olsun.
-button tıklaması ile şifre istiyor. şifre ynalışsa registry dosyalarını sil.
-şifre doğru ise button visible'sini true yap.
-başka bir button tıklaması ile array liste veri aktarıcaz.
-dll ile ortalama hesapla ve textBoxa yaz.
-button tıklaması ile veritabanına ekle.
-Button tıklaması ile rastgele bir kişinin bilgileirni textBoxlara yazdır.
-*/
-        // DLL İÇERİĞİ 
+            // DLL İÇERİĞİ 
         /*
          * public class Hesap
          * {
@@ -38,9 +27,11 @@ Button tıklaması ile rastgele bir kişinin bilgileirni textBoxlara yazdır.
          *      }
          *  }
          */
-        SqlConnection con = new SqlConnection("DataSource=Destkop;inital Catalog=tablo");
+    
+    public partial class Form1 : Form
+    {
         Hesap hsb = new Hesap();
-        bool aktif = false;
+        SqlConnection con = new SqlConnection();
         int sifre = 2001;
 
         public Form1()
@@ -48,60 +39,52 @@ Button tıklaması ile rastgele bir kişinin bilgileirni textBoxlara yazdır.
             InitializeComponent();
         }
 
-        void textler()
-        {
-            textBox1.Visible = aktif;
-            textBox2.Visible = aktif;
-            textBox3.Visible = aktif;
-            textBox4.Visible = aktif;
-            textBox5.Visible = aktif;
-            textBox6.Visible = aktif;
-            textBox7.Visible = aktif;
-            textBox8.Visible = aktif;
-            textBox9.Visible = aktif;
-            textBox10.Visible = aktif;
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
-            Registry.CurrentUser.CreateSubKey("dosya1").CreateSubKey("dosya2").SetValue("Bora", sifre);
-            textler();
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            textBox3.Visible = false;
+            textBox4.Visible = false;
+            textBox5.Visible = false;
+
+            Registry.CurrentUser.CreateSubKey("dosya1").CreateSubKey("dosya2").SetValue("emre", sifre);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int sifre = int.Parse(Interaction.InputBox("Sifre", ""));
-            if (sifre != (int)Registry.CurrentUser.OpenSubKey("dosya1").OpenSubKey("dosya2").GetValue("Bora"))
-                Registry.CurrentUser.DeleteSubKeyTree("dosya1");
-            else
+            int kullanici = int.Parse(Interaction.InputBox("Şifre gir", "Sifre"));
+            if (sifre == kullanici)
             {
-                aktif = true;
-                textler();
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox3.Visible = true;
+                textBox4.Visible = true;
+                textBox5.Visible = true;
             }
+            else Registry.CurrentUser.DeleteSubKeyTree("dosya1");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             ArrayList al = new ArrayList();
+
             al.Add(textBox1.Text);
             al.Add(textBox2.Text);
             al.Add(textBox3.Text);
             al.Add(textBox4.Text);
             al.Add(textBox5.Text);
-            al.Add(textBox6.Text);
-            al.Add(textBox7.Text);
-            al.Add(textBox8.Text);
-            al.Add(textBox9.Text);
-            al.Add(textBox10.Text);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            if (textBox10.Visible == true)
-                textBox10.Text = hsb.ortalama(int.Parse(textBox8.Text), int.Parse(textBox9.Text));
+            int vize = Convert.ToInt32(textBox3.Text);
+            int final = Convert.ToInt32(textBox4.Text);
+
+            textBox5.Text = hsb.ortalama(vize, final).ToString();
 
             con.Open();
-            SqlCommand cmd = new SqlCommand("Insert into ogrenci Values(" + textBox1.Text + "," + textBox2.Text + ",'" + textBox3.Text + "'," + textBox4.Text + "," + textBox5.Text + "," + textBox6.Text + "," + textBox7.Text + "," + textBox8.Text + "," + textBox9.Text + "," + textBox10.Text + ")", con);
+            string cumle = String.Format("Insert into tablo values({0},'{1}',{2},{3},{4})", textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+            SqlCommand cmd = new SqlCommand(cumle, con);
             cmd.ExecuteNonQuery();
             con.Close();
         }
@@ -109,24 +92,18 @@ Button tıklaması ile rastgele bir kişinin bilgileirni textBoxlara yazdır.
         private void button4_Click(object sender, EventArgs e)
         {
             con.Open();
-            SqlCommand cmd = new SqlCommand("Select * from ogrenci where id=1",con);
+            SqlCommand cmd = new SqlCommand("Select * from tablo where id=1", con);
             SqlDataReader dr = cmd.ExecuteReader();
+
             if(dr.Read())
             {
                 textBox1.Text = dr[0].ToString();
-                textBox1.Text = dr[1].ToString();
-                textBox1.Text = dr[2].ToString();
-                textBox1.Text = dr[3].ToString();
-                textBox1.Text = dr[4].ToString();
-                textBox1.Text = dr[5].ToString();
-                textBox1.Text = dr[6].ToString();
-                textBox1.Text = dr[7].ToString();
-                textBox1.Text = dr[8].ToString();
-                textBox1.Text = dr[9].ToString();
-                textBox1.Text = dr[10].ToString();
+                textBox2.Text = dr[1].ToString();
+                textBox3.Text = dr[2].ToString();
+                textBox4.Text = dr[3].ToString();
+                textBox5.Text = dr[4].ToString();
             }
             con.Close();
-
         }
     }
 }

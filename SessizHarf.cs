523309de9fb86bb74cc17900139e252fcf2db4da
Bora Sapancılar <114573591+barbo7@users.noreply.github.com>
2023-03-23 04:03:56 +0300

    Update SessizHarf.cs

--- SessizHarf.cs
+++ SessizHarf.cs
@@ -2,7 +2,6 @@
  * Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
 
 Örnek: Input: Merhaba Umut Arya
-
 Output: True False True
 */
 
